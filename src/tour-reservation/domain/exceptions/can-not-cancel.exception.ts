a7f85c6b12c72e49573b8b3f839e7dc0e910da92
swpark6 <sewon1234@naver.com>
2023-12-30 09:15:01 +0900

    feat: add http exception filter

--- src/tour-reservation/domain/exceptions/can-not-cancel.exception.ts
+++ src/tour-reservation/domain/exceptions/can-not-cancel.exception.ts
@@ -1,4 +1,6 @@
-export class CanNotCancelException extends Error {
+import { ConflictException } from '@nestjs/common';
+
+export class CanNotCancelException extends ConflictException {
   constructor(now: Date, cancellationDueDate: Date) {
     super(
       `now=${now.toISOString()}, cancellationDueDate=${cancellationDueDate.toISOString()}`,
